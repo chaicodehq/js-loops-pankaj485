c1852b453f294cf1caa477fade8ba3b35bf696ed
pankaj485 <pankajsamar2@gmail.com>
2026-03-10 14:02:10 +0530

    complete assignment 7

--- src/07-ipl-points-table.js
+++ src/07-ipl-points-table.js
@@ -38,4 +38,88 @@
  */
 export function iplPointsTable(matches) {
   // Your code here
+
+  if (!Array.isArray(matches) || matches.length === 0) {
+    return [];
+  }
+
+  const data = [];
+
+  /*
+   *   // CSK: played=2, won=1, tied=1, points=3
+   *   // MI: played=1, won=0, lost=1, points=0
+   *   // RCB: played=1, tied=1, points=1
+   *   // Sorted: CSK(3), RCB(1), MI(0)
+   */
+
+  matches.forEach((match) => {
+    const { result, team1, team2, winner } = match;
+
+    // team: string, played: number, won: number, lost: number, tied: number, noResult: number, points: number
+
+    const firstTeamData = {
+      team: team1,
+      played: 1,
+      noResult: result === "no_result" ? 1 : 0,
+      tied: result === "tie" ? 1 : 0,
+      lost: result === "win" && winner !== team1 ? 1 : 0,
+      won: result === "win" && winner === team1 ? 1 : 0,
+      points: result === "win" ? (winner === team1 ? 2 : 0) : 1,
+    };
+
+    const secondTeamData = {
+      team: team2,
+      played: 1,
+      noResult: result === "no_result" ? 1 : 0,
+      tied: result === "tie" ? 1 : 0,
+      lost: result === "win" && winner !== team2 ? 1 : 0,
+      won: result === "win" && winner === team2 ? 1 : 0,
+      points: result === "win" ? (winner === team2 ? 2 : 0) : 1,
+    };
+
+    const firstTeamDataIndex = data.findIndex((_) => _.team === team1);
+    const secondTeamDataIndex = data.findIndex((_) => _.team === team2);
+
+    if (firstTeamDataIndex === -1) {
+      data.push(firstTeamData);
+    } else {
+      const currentData = structuredClone(data[firstTeamDataIndex]);
+
+      data[firstTeamDataIndex] = {
+        ...currentData,
+        played: currentData.played + 1,
+        noResult: currentData.noResult + firstTeamData.noResult,
+        tied: currentData.tied + firstTeamData.tied,
+        lost: currentData.lost + firstTeamData.lost,
+        won: currentData.won + firstTeamData.won,
+        points: currentData.points + firstTeamData.points,
+      };
+    }
+
+    if (secondTeamDataIndex === -1) {
+      data.push(secondTeamData);
+    } else {
+      const currentData = structuredClone(data[secondTeamDataIndex]);
+
+      data[secondTeamDataIndex] = {
+        ...currentData,
+        played: currentData.played + 1,
+        noResult: currentData.noResult + secondTeamData.noResult,
+        tied: currentData.tied + secondTeamData.tied,
+        lost: currentData.lost + secondTeamData.lost,
+        won: currentData.won + secondTeamData.won,
+        points: currentData.points + secondTeamData.points,
+      };
+    }
+  });
+
+  const sortedData = [...data].sort(
+    (a, b) => b.points - a.points || a.team.localeCompare(b.team),
+  );
+
+  return sortedData;
 }
+
+const result = iplPointsTable([{ team1: "RCB", team2: "DC", result: "tie" }]);
+
+console.table(result);
